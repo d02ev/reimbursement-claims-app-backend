a195045fe542583acb4a45fdf7ffb6f76a733606
d02ev <vikramadityapratapsingh98@gmail.com>
2024-03-16 21:51:41 +0530

    add: .gitignore and dotenv config

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 app.use(passport.initialize())
 
 app.use('/api/auth', authRoutes)
