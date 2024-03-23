82d3e4a4e9a17e577e8757ed3630f7678a34ffec
d02ev <vikramadityapratapsingh98@gmail.com>
2024-03-24 00:07:01 +0530

    remove: passport js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
-import passport from 'passport';
 
 import initServer from './config/server.config';
-import jwtStrategy from './auth/strategy/jwt.strategy';
 import errorHandler from './middlewares/errorHandler.middleware';
 
 import authRoutes from './routes/auth.routes';
@@ -18,13 +16,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(passport.initialize());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/claims', claimRoutes);
 
 app.use(errorHandler);
 
-jwtStrategy(passport);
 initServer(CONNECTION_PORT, app);
